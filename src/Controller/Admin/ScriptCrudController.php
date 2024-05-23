3f5a496514bd5cfe2464a56041eb22cdd99d0d45
kylian <kylian.omahony1@gmail.com>
2024-05-23 11:40:33 +0200

    avancement

---
+++ src/Controller/Admin/ScriptCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Script;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class ScriptCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Script::class;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
